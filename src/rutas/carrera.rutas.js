5bb0670e3df3815883a5ea083c936cc5b54f7c09
Santiago104034 <victor.ferrer@iteso.mx>
2026-05-16 14:39:55 -0600

    Termino de proyecto

--- src/rutas/carrera.rutas.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const carreraController = require('../controladores/carrera.controlador');
-
-// Esta ruta queda pública para poder obtener el id de carrera al registrarse.
-router.get('/', carreraController.listCareers);
-
-module.exports = router;
